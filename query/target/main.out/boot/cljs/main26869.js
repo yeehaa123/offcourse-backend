38d8954de40977983c618997d743531255a7e11c
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-09-15 01:04:33 +0200

    wip: fix gitignore

--- query/target/main.out/boot/cljs/main26869.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.89 {:target :nodejs}
-goog.provide('boot.cljs.main26869');
-goog.require('cljs.core');
-goog.require('adzerk.boot_cljs_repl');
-goog.require('app.core');
